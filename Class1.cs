d7b850ff97698a33b3eadcc0b1931aa04f08e190
Abhibhardiya24 <abhishekbhardiya1995@gmail.com>
2023-08-31 18:27:13 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿namespace LargestToThreeNumbers
+{
+    public class Class1
+    {
+        class Pencil
+
+
+
+        {
+
+
+
+            static void Main()
+
+
+
+            {
+
+
+
+                int num1 = 60;
+
+
+
+                int num2 = 45;
+
+
+
+                int num3 = 123;
+
+
+
+                if (num1 > num2)
+
+
+
+                {
+
+
+
+                    //{ Console.WriteLine(num2);}
+
+
+
+                    if (num2 < num3)
+
+
+
+                    { Console.WriteLine(num3); }
+
+
+
+                }
+
+
+
+                else { Console.WriteLine("Wrong Input"); }
+
+
+
+                System.Console.ReadKey();
+
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
